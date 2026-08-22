cdddda60fabd21bc7a5680bc2121ec4c37526638
Habib Thiaw <thiawhabib99@gmail.com>
2026-08-22 03:12:31 +0200

    troisieme

---
+++ projets/presentation.py
@@ -0,0 +1,8 @@
+nom = input("Nom : ")
+ville = input("Ville : ")
+objectif = input("Objectif : ")
+
+print("\n===== Ma présentation =====")
+print(f"Nom : {nom}")
+print(f"Ville : {ville}")
+print(f"Objectif : {objectif}")
