6f5647be3f12d36b9033e513ef61f0c69da0d327
Habib Thiaw <thiawhabib99@gmail.com>
2026-08-23 18:53:18 +0200

    jour3 debugger

---
+++ debug_jour3.py
@@ -0,0 +1,6 @@
+nom = input("Nom : ")
+
+age = int(input("Âge : "))
+
+print("Bonjour " + nom)
+print("Vous avez " + str(age) + " ans.")
---
+++ presentation_v2.py
@@ -0,0 +1,8 @@
+nom=input("Nom : ")
+age=int(input("Age: "))
+ville=input("ville:")
+
+print("\n===== Carte d'identité =====")
+print(f"Nom :{nom}")
+print(f"Age :{age}")
+print(f"Ville :{ville}")
